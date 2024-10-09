4a36f6bdf3d16f79107425e8d92da614fcc9f10b
Vinicius Moralis <vinicius_moralis@hotmail.com>
2024-10-09 17:20:22 -0300

    feat: fixing first page

--- modules/home/screens/HomePage.tsx
+++ modules/home/screens/HomePage.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { StackParamScreensList } from "@/app/navigation/StackNavigator";
 
@@ -6,18 +6,20 @@ const HomePage = () => {
   const navigation = useNavigation<NavigationProp<StackParamScreensList>>();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Are you ready to change the way you see money?</Text>
-      <Text style={styles.subtitle}>So dive into it</Text>
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.registerButton} onPress={() => { navigation.navigate("Register") }}>
-          <Text style={styles.registerText}>Register</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginButton}>
-          <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
+    <ImageBackground style={styles.imageContainer} source={require('@/ds/assets/images/fintech-app-background-2.jpeg')}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Are you ready to change the way you see money?</Text>
+        <Text style={styles.subtitle}>Just dive into</Text>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.registerButton} onPress={() => { navigation.navigate("Register") }}>
+            <Text style={styles.registerText}>Register</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   )
 }
 
@@ -26,7 +28,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-between",
     alignItems: "center",
-  }, 
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 48
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    resizeMode: "cover",
+  },
   buttonsContainer: {
     gap: 40,
     flexDirection: "row",
@@ -38,36 +47,40 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     textTransform: "uppercase",
-    textAlign: "center"
+    textAlign: "center",
+    color: "#FFF"
   },
   subtitle: {
     fontSize: 24,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    color: "#FFF"
   },
   registerButton: {
     borderRadius: 24,
     alignItems: "center",
     justifyContent: "center",
     height: 48,
-    width: 96,
+    width: 104,
     backgroundColor: "#1A9F59"
   },
   registerText:{
     color: "#FFF",
-    fontSize: 16
+    fontSize: 16,
+    fontWeight: "500"
   },
   loginButton: {
     borderRadius: 24,
     alignItems: "center",
     justifyContent: "center",
     height: 48,
-    width: 96,
-    borderColor: "#1A9F59",
-    borderWidth: 2,
+    width: 104,
+    borderColor: "#1ADF59",
+    borderWidth: 2.5
   },
   loginText:{
-    color: "#1A9F59",
-    fontSize: 16
+    color: "#1ADF59",
+    fontSize: 16,
+    fontWeight: "500"
   }  
 })
 
